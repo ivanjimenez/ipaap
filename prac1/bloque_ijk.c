263c56246ae5f69c845b31879fa34b026831b23c
ivanjimenez <ivan.jimenezster@gmail.com>
2013-11-26 19:00:18 -0300

    firs

--- prac1/bloque_ijk.c
+++ prac1/bloque_ijk.c
@@ -64,13 +64,12 @@ for(i=0;i<n;i++)
 Orden bucle ijk */ 	    
 
 inicio = clock();
-
-for (ib=0; ib<n; ib++)
-	for (i = (ib-1) * alfa + 1; i==ib * alfa; i++)
-		for(jb=0; jb<n; jb++)
-			for(j = (jb-1) * alfa + 1;j==jb*alfa;j++)
-				for(kb=0; kb++; kb<n)					
-					for(k = (kb-1) * alfa+1;k==jb*alfa;k++)
+for (i = (ib) * alfa; i==ib * alfa; i++)
+	for(j = (jb) * alfa;j==jb*alfa;j++)
+		for(k = (kb) * alfa;k==jb*alfa;k++)
+			for (ib=0; ib<n; ib++)
+				for(jb=0; jb<n; jb++)
+					for(kb=0; kb++; kb<n)					
 			   	 	   M(C,i,j,ldc) = M(C,i,j,ldc)+(M(A,i,k,lda) * M(B,k,j,ldb));
                   
 fin = clock();
