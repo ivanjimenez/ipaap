91507b5619ce4136abfb329f83be9ba3709807c8
ivanjimenez <ivan.jimenezster@gmail.com>
2013-12-01 19:08:20 -0300

    second

--- prac1/blas_dgemv.c
+++ prac1/blas_dgemv.c
@@ -10,15 +10,13 @@ int main(int argc, char **argv)
 	
 	int result;
 	int i,j,l;
-	int m,n,lda,ldx,ldy;
-	double *A,*X,*Y;
-	
+	int m,n,k,lda,ldb,ldc;
+	double *A,*B,*C;
 
 	clock_t inicio, fin;
 	double  duration;
 	
-/* Comprobación número de argumentos correctos. Se pasaran m n k */
-
+/* Comprobación número de argumentos correctos. Se pasaran m n */
 if (argc!=3)
    {
    printf("Error de Sintaxis. Uso: blas_dgemv m n\n");
@@ -26,44 +24,55 @@ if (argc!=3)
    }
 
 /* Lectura de parametros de entrada */
-   m=atoi(argv[1]); n=atoi(argv[2]); 
-   
-/* Dimensionado de las matrices, utilizando funciones propias */
-   lda=m; ldx=n; ldy=n;
-   A=dmatrix(m,m); X=dmatrix(n,n); Y=dmatrix(n,n);
+m=atoi(argv[1]); n=atoi(argv[2]); 
 
+/* Dimensionado de las matrices, utilizando funciones propias */
+lda=m; ldb=n; ldc=n;
+A=dmatrix(m,m); B=dmatrix(n,n); C=dmatrix(n,n);
 
 /* Relleno de las matrices con valores aleatorios. Uso de macro propia */
-   
-   //Matriz A
 
-   for (i=0;i<m;i++)
-	   for(j=0;i<m;j++)
-		   M(A,i,j,lda) = rand() % 20;
+//matriz A
 
-	//Vector X
-   
-   for (i=0;i<n;i++)
-	   for(j=0;i<n;j++)
-		   M(X,i,j,ldx) = rand() % 20;  
-    
-   //Vector Y
-   
-   for (i=0;i<n;i++)
-	   for(j=0;i<n;j++)
-		   M(Y,i,j,ldy) = rand() % 20;
+for(i=0;i<m;i++)
+	for(j=0;j<m;j++) {
+		M(A,i,j,lda) = rand() % 20;
+		
+	}
+		
+	
+//matriz B
 
-// Computa la operaciÃ³n: cblas.dgemmv; y <- alfa*op(A)*x + beta*y
+for(i=0;i<n;i++)
+	for(j=0;j<n;j++){
+		
+		M(B,i,j,ldb) = rand() % 20;
+		
+	}
+		
+
+//matriz C
+ 
+for(i=0;i<n;i++)
+	for(j=0;j<n;j++) {
+		M(C,i,j,ldc) = rand() % 100;
+		
+	}
+		
+
+
+// Computa la operacion: C <- alfa * op(A) * X * + beta*Y
     
 
 inicio = clock();
 
-cblas_dgemv(CblasRowMajor,CblasNoTrans,m,n,1.0,A,lda,X,1,0.0,Y,1);
+cblas_dgemv(CblasColMajor,CblasNoTrans,m,n,1.0,A,lda,B,1,0.0,C,1);
                   
 fin = clock();
 duration = (double)(fin - inicio) / CLOCKS_PER_SEC;
-printf("%d %d\n", m, n);
-printf("alfa*op(A)*V + beta*Y: %2.5f segundos\n", duration );
+printf("%d %d %d\n", m, n, k);
+printf("C <- alfa * op(A) * X * + beta*Y: %2.5f segundos\n", duration );
 
 
 }
+
