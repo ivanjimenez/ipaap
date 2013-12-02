4ef7af39ba27defe5e3d7f7af7c2494d48820083
ivanjimenez <ivan.jimenezster@gmail.com>
2013-12-02 11:41:06 -0300

    second

--- prac1/blas_dgemv.c
+++ prac1/blas_dgemv.c
@@ -63,7 +63,7 @@ for(i=0;i<m;i++)
 inicio = clock();
 
 for(j=0;j<n;j++)
-	cblas_dgemv(CblasRowMajor,CblasNoTrans,m,n,1.0,A,lda,B,1,0.0,C,1);
+	M(C,i,j,ldc) = cblas_dgemv(CblasRowMajor,CblasNoTrans,m,n,1.0,A,lda,B,1,0.0,C,1);
                   
 fin = clock();
 duration = (double)(fin - inicio) / CLOCKS_PER_SEC;
