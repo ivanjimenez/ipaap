91a898ac4700c70c740bc7ef5ff3c215641b10cc
ivanjimenez <ivan.jimenezster@gmail.com>
2013-12-02 10:21:40 -0300

    second

--- prac1/bloque_jki.c
+++ prac1/bloque_jki.c
@@ -68,9 +68,9 @@ inicio = clock();
 for(ib=0; ib<n; ib++)
 	for(jb=0; jb<n; jb++)
 		for(kb=0; kb<n; kb++)			
-				for(j = jb * alfa; j< jb * alfa; j++)
-					for(k = kb * alfa; k < kb * alfa; k++) 
-						for(i = ib * alfa; i< ib * alfa; i++)
+				for(j = (jb + 1) * alfa; j< jb * alfa; j++)
+					for(k = (kb +1 ) * alfa; k < kb * alfa; k++) 
+						for(i = (ib + 1) * alfa; i< ib * alfa; i++)
 						M(C,i,j,ldc) = M(C,i,j,ldc)+(M(A,i,k,lda) * M(B,k,j,ldb));
 				 	   
                   
