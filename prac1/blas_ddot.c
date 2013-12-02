f33b4f2342f8a18f55245f6be7249fad0ed838d6
ivanjimenez <ivan.jimenezster@gmail.com>
2013-12-02 10:49:42 -0300

    second

--- prac1/blas_ddot.c
+++ prac1/blas_ddot.c
@@ -33,9 +33,8 @@ int main(int argc, char **argv)
 	/* Relleno de las matrices con valores aleatorios. Uso de macro propia */
 
 	//matrixA 
-
 	for(i=0;i<n;i++)
-		for(j=0;j<n;j++) 
+		for(j=0;j<n;j++)
 			M(A,i,j,lda) = rand() % 20;		
 	
 	//matrizB 
