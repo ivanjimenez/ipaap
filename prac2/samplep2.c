54b0718f61120e4eb5a58a736fb313f6601246bb
ivanjimenez <ivan.jimenezster@gmail.com>
2013-12-02 19:46:29 -0300

    second

---
+++ prac2/samplep2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+int main(int argc, char *argv[]) {
+	
+	int tid;
+	printf("hello world from threads:\n");
+	
+	#pragma omp parallel private(tid) 
+	{
+	tid = omp_get_thread_num();
+	printf("<%d>\n",tid);
+	
+	}
+	printf("I am secquential now\n");
+	return 0;
+	
+}
+
+
