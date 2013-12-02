f871f9fd9f12e9c9f151a897782db6604bdcf314
ivanjimenez <ivan.jimenezster@gmail.com>
2013-12-02 20:08:46 -0300

    second

--- prac2/samplep2.c
+++ prac2/samplep2.c
@@ -1,6 +1,12 @@
+/* Compile with: gcc samplep2.c -o samplep2 -fopenmp
+ Sample for Magister Thesis
+ by Iván Jiménez
+	 */
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
+
+
 
 int main(int argc, char *argv[]) {
 	
---
+++ prac2/samplep3.c
@@ -0,0 +1,18 @@
+/* Compile with: gcc samplep2.c -o samplep2 -fopenmp
+ Sample for Magister Thesis
+ by Iván Jiménez
+	 */
+#include <stdio.h>
+#include <stdlib.h>
+#include "mpi.h"
+
+
+
+int main(int argc, char *argv[]) {
+	
+	
+	
+
+}
+
+
