250534269e474ee980bd0fad1b5773f302645345
ivanjimenez <ivan.jimenezster@gmail.com>
2013-12-03 17:31:30 -0300

    second

--- prac1/mpi_blas_gemm.c
+++ prac1/mpi_blas_gemm.c
@@ -35,7 +35,7 @@ int main(int argc, char *argv[]) {
 	MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 	
-	if (my_rank != 0) {
+	if (my_rank == 0) {
 		sprintf(greeting, "Greetings from process %d of %d!", my_rank, comm_sz);
 		MPI_Send(greeting, strlen(greeting)+1, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
 		
