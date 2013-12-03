5611070d635b5ba891ce3dc267168d41f736d752
ivanjimenez <ivan.jimenezster@gmail.com>
2013-12-03 15:48:25 -0300

    second

--- prac1/mpi_blas_gemm.c
+++ prac1/mpi_blas_gemm.c
@@ -1,33 +1,36 @@
 /* Compile with: gcc samplep2.c -o samplep2 -fopenmp
  Sample for Magister Thesis
  by Iván Jiménez
-	 */
+	 
+   # Crear N procesos (N<=8)
+   # El P0 se encarga de dividir la matriz B en N-1 bloques. 
+   # Cada bloque: conjunto columnas consecutivas de B
+   # P0 debe enviar al resto de procesos i<>0 la matriz A completa y la parte B que corresponda, utilizando las primitivas MPI
+   # Cada proceso i debe realizar el producto de matrices utilizando cblas_dgemm 
+   devolviendo el resultado a P0.
+   # Finalmente, P0 recibe los resultados parciales y muestra la matriz C resultado
+
+
+    ##Build:
+	# make -f Makefile_mpi_blas mpi_blas_gemm
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "mpi.h"
 
-
+const int MAX_STRING = 100;
 
 int main(int argc, char *argv[]) {
 	
-	/* Crear N procesos (N<=8)
-	   El P0 se encarga de dividir la matriz B en N-1 bloques. 
-	   Cada bloque: conjunto columnas consecutivas de B
-	   P0 debe enviar al resto de procesos i<>0 la matriz A completa y la parte B que corresponda, utilizando las primitivas MPI
-	   Cada proceso i debe realizar el producto de matrices utilizando cblas_dgemm 
-	   devolviendo el resultado a P0.
-	   Finalmente, P0 recibe los resultados parciales y muestra la matriz C resultado
-	*/
-	
-	/*   Build:
-		 make -f Makefile_mpi_blas mpi_blas_gemm
-	*/
-
-
 	/* Proceso 0 divide en np-1 bloques matriz a*/
 	
-	printf("This is a test");
-	return(0);
+	char greeting[MAX_STRING];
+	int comm_sz;
+	int my_rank;
+	
+	MPI_Init(NULL, NULL);
+	MPI_Comm_size(MPI_COMM_WORLD,&comm_sz);
 
 }
 
