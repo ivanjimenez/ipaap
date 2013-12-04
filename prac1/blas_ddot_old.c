2ab0d7556bfb8be843204b8b49190581a44a4e5f
ivanjimenez <ivan.jimenezster@gmail.com>
2013-12-04 12:13:06 -0300

    second

--- prac1/blas_ddot_old.c
+++ prac1/blas_ddot_old.c
@@ -1,62 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "memoryfun.h"
+//#include "memoryfun.h"
 #include "cblas.h" 
 
 
 int main(int argc, char **argv)
 {
 	
-	int result;
-	int i,j,l;
-	int m,n,k,lda,ldb,ldc;
-	double *A,*B,*C;
+    double  m[10],n[10];
+          int i;
+          int result;
 
-	clock_t inicio, fin;
-	double  duration;
-	
-	/* Comprobación número de argumentos correctos. Se pasaran m n k */
-	if (argc!=2)
-	   {
-	   printf("Error de Sintaxis. Uso: blas_ddot n \n");
-	   exit(1);
-	   }
+          printf("Enter the elements into first vector.\n");
+          for(i=0;i<10;i++)
+          scanf("%lf",&m[i]);
 
-	/* Lectura de parametros de entrada */
-	n=atoi(argv[1]);
+          printf("Enter the elements into second vector.\n");
+          for(i=0;i<10;i++)
+          scanf("%lf",&n[i]);
 
-	/* Dimensionado de las matrices, utilizando funciones propias */
-	lda=n; ldb=n; ldc=n; 
-	A=dmatrix(n,n); B=dmatrix(n,n); C=matrix(n,n)
+          result = ddot(m,n);
+          printf("The result is %d\n",result);
 
-	/* Relleno de las matrices con valores aleatorios. Uso de macro propia */
-
-	//matrixA 
-
-		for(i=0;i<n;i++)
-			for(j=0;j<n;j++)
-				M(A,i,j,lda) = rand() % 20;
-	
-	//matrizB 
-
-	for(i=0;i<n;i++)
-		for(j=0;j<n;j++)
-			M(B,i,j,ldb) = rand() % 20;
-				
-	
-// Computa la operación: cblas.dot <- X^T*Y
-
-inicio = clock();
-
-for(i=0;i<n;i++)
-	for(j=0;j<n;j++)
-	   M(C,i,j,ldc) = cblas_ddot(n,A,i,B,j);
-                  
-fin = clock();
-duration = (double)(fin - inicio) / CLOCKS_PER_SEC;
-printf("%d %d\n", n, result);
-printf("x^T*y: %2.5f segundos\n", duration );
+          return 0;
 
 
 }
+
