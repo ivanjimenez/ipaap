52f5388d5f1b50227ac65c288b2d3612879aa6da
ivanjimenez <ivan.jimenezster@gmail.com>
2013-12-04 12:19:52 -0300

    second

--- prac1/blas_ddot_old.c
+++ prac1/blas_ddot_old.c
@@ -13,11 +13,11 @@ int main(int argc, char **argv)
           int result;
 
           printf("Enter the elements into first vector.\n");
-          for(i=0;i<10;i++)
+          for(i=0;i<5;i++)
           scanf("%lf",&m[i]);
 
           printf("Enter the elements into second vector.\n");
-          for(i=0;i<10;i++)
+          for(i=0;i<5;i++)
           scanf("%lf",&n[i]);
 
           result = cblas_ddot(5, m, 1, n, 1);
