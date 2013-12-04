f65c85622465eb0b935e14c5cf2ff38a1a498390
ivanjimenez <ivan.jimenezster@gmail.com>
2013-12-04 13:21:06 -0300

    second

--- prac1/blas_ddot.c
+++ prac1/blas_ddot.c
@@ -57,7 +57,7 @@ inicio = clock();
 
 for(i=0;i<n;i++)
 	for(j=0;j<n;j++)
-		M(C,i,j,ldc) = cblas_ddot(n,A,lda,B,1);
+		M(C,i,j,ldc) = cblas_ddot(n,A+i,lda,B+j*ldb,1);
 		 
                   
 fin = clock();
