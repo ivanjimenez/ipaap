efc8e3cdbdfa7c0753f9e7f40c5d740c3e905df4
ivanjimenez <ivan.jimenezster@gmail.com>
2013-12-05 16:58:56 -0300

    second

--- prac1/mpi_blas_gemm.c
+++ prac1/mpi_blas_gemm.c
@@ -106,6 +106,7 @@ int main(int argc, char *argv[]) {
 			  //Enviamos la parte de Matriz B que corresponda
 			  MPI_Send(B+bloqueTam * n * (i-1),bloqueTam * n,MPI_DOUBLE,i, 0, MPI_COMM_WORLD);
 		  	  MPI_Recv(C+bloqueTam * n * (i-1),bloqueTam * n,MPI_DOUBLE,mid,0,MPI_COMM_WORLD,&st);
+		      //Imprimir Resultado Matriz Proceso 0
 		  }
 	      
 	}
