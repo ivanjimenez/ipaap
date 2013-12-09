729901fe80b628f6cc06fe00ea08b4d5f2d7d0ab
ivanjimenez <ivan.jimenezster@gmail.com>
2013-12-09 09:21:32 -0300

    monday

--- prac1/bloque_ijk_old.c
+++ prac1/bloque_ijk_old.c
@@ -1,66 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 #include "memoryfun.h"
 //#include "cblas"
 
 
 int main(int argc, char **argv)
 {
-int i,j,l, alfa;
-int m,n,k,lda,ldb,ldc;
+int i,j,k,ib,jb,kb;
+int alfa;  //tama de bloque
+int n,lda,ldb,ldc;
 double *A,*B,*C;
 
 clock_t inicio, fin;
 double  duration;
 	
-/* Comprobación número de argumentos correctos. Se pasarán m n k */
-if (argc!=5)
+/* Comprobación número de argumentos correctos. Se pasaran m n k */
+if (argc!=3)
    {
-   printf("Error de Sintaxis. Uso: mi_gemm_ijk m n k\n");
+   printf("Error de Sintaxis. Uso: bloque_ijk n alfa\n");
    exit(1);
    }
 
-/* Lectura de parámetros de entrada */
-m=atoi(argv[1]); n=atoi(argv[2]); k=atoi(argv[3]); alfa=atoi(argv[4]);
+/* Lectura de parametros de entrada */
+n=atoi(argv[1]); alfa=atoi(argv[2]);
 
 /* Dimensionado de las matrices, utilizando funciones propias */
-lda=m; ldb=k; ldc=m;
-A=dmatrix(m,k); B=dmatrix(k,n); C=dmatrix(m,n);
+lda=n; ldb=n; ldc=n;
+A=dmatrix(n,n); B=dmatrix(n,n); C=dmatrix(n,n);
 
 /* Relleno de las matrices con valores aleatorios. Uso de macro propia */
 
 //matriz A
 
-for(i=0;i<m;i++)
-	for(j=0;j<k;j++) {
+for(i=0;i<n;i++)
+	for(j=0;j<n;j++) {
 		M(A,i,j,lda) = rand() % 20;
-		//printf("M(%d,%d)=%d\n", i, j,M(A,i,j,lda));	
+			
 	}
 		
 	
 //matriz B
 
-for(i=0;i<k;i++)
+for(i=0;i<n;i++)
 	for(j=0;j<n;j++){
 		
 		M(B,i,j,ldb) = rand() % 20;
-		// printf("M(%d,%d)=%d\n", i, j,M(B,i,j,ldb));
+		
 	}
 		
 
 //matriz C
 
-for(i=0;i<m;i++)
+for(i=0;i<n;i++)
 	for(j=0;j<n;j++) {
-		M(C,i,j,ldc) = rand() % 100;
-		//printf("M(%d,%d)=%d", i, j,M(C,i,j,ldc));
+		M(C,i,j,ldc) = rand() % 20;
+		
 	}
-	
+		
 
 
-/* producto de matrices */ 	    
+/* producto de matrices BLOQUE
+Orden bucle ijk */ 	    
+
 inicio = clock();
 
 for (ib = 0; ib < n; ib++) 
@@ -71,12 +73,10 @@ for (ib = 0; ib < n; ib++)
 					for (k = kb * alfa; kb < (kb +1)*alfa;k++)
 						M(C,i,j,ldc) = M(C,i,j,ldc) + (M(A,i,k,lda) * M(B,k,j,ldb));
 
-
-     
 fin = clock();
 duration = (double)(fin - inicio) / CLOCKS_PER_SEC;
-printf("%d %d %d %d\n", m, n, k, alfa);
-printf("C=AB (bloque_ijk): %2.1f segundos\n", duration );
+printf("%d %d \n", n, alfa);
+printf("Bloques:: C=AB (ijk): %2.5f segundos\n", duration );
 
 
 }
