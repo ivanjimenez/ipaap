b1dcc26d344fc301304ce1e51b4504583dd71f9a
ivanjimenez <ivan.jimenezster@gmail.com>
2014-01-17 14:10:56 -0300

    prac3

--- prac3/matprod.c
+++ prac3/matprod.c
@@ -13,6 +13,8 @@ by Iván Jiménez
 #include "memoryfun.h"
 #include "mpi.h"
 
+
+
 int main(int argc, char *argv[]) {
         
 /***** Declaración de variables para la parte de declaración de matrices ******/
@@ -90,9 +92,8 @@ int main(int argc, char *argv[]) {
         
         if ((myprow==0) && (mypcol==0)){
                 
-           	/**** Relleno de las matrices con valores aleatorios. Uso de macro propia */
-		   
-		   	A=dmatrix(m,n); B=dmatrix(m,n); C=dmatrix(m,n);
+            A=dmatrix(m,n); B=dmatrix(m,n); C=dmatrix(m,n);
+        /**** Relleno de las matrices con valores aleatorios. Uso de macro propia */
 
                 //matriz A
 
@@ -100,7 +101,7 @@ int main(int argc, char *argv[]) {
                         for(j=0;j<n;j++) {
                                 M(A,i,j,lda) = rand() % 20;
                         }
-						                
+                
                 //matriz B
 
                 for(i=0;i<m;i++)
@@ -114,6 +115,7 @@ int main(int argc, char *argv[]) {
                         for(j=0;j<n;j++){
                                 M(C,i,j,ldc) = rand() % 20;
                         }
+                
         }
                 
         Cblacs_exit(); //cerramos blacs
