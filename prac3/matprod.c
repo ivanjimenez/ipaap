7fd047f8c89d90330267e685c8ad63fec7733edc
ivanjimenez <ivan.jimenezster@gmail.com>
2014-01-24 09:06:45 -0300

    prac3

--- prac3/matprod.c
+++ prac3/matprod.c
@@ -231,6 +231,10 @@ int main(int argc, char *argv[]) {
 			for (i=0;i<n_filas_locales;i++)
 				for(j=0;j<n_col_locales;j++)
 					printf("Alocal[%d,%d]= %f\n",i,j,M(A,i,j,lda));	
+			
+			for (i=0;i<n_filas_locales;i++)
+				for(j=0;j<n_col_locales;j++)
+					printf("Blocal[%d,%d]= %f\n",i,j,M(B,i,j,ldb));	
 		
 			
 		}
